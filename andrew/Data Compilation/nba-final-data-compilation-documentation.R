7e66f1d31fcf707e1af25ad23e396471846bef62
Andrew Noecker <noecke2@stolaf.edu>
2022-07-15 15:12:22 -0400

    Pulling raw totals and updating cumulative dataset

--- andrew/Data Compilation/nba-final-data-compilation-documentation.R
+++ andrew/Data Compilation/nba-final-data-compilation-documentation.R
@@ -12,6 +12,28 @@ nba_pbp <- read_csv("data/nba_pbp.csv")
 nba_shooting <- read_csv("data/nba_shooting.csv")
 nba_per100_stats <- read_csv("data/nba_per100_stats.csv")
 nba_adv_stats <- read_csv("data/nba_adv_stats.csv")
+nba_total_stats <- read_csv("data/nba_total_stats.csv")
+
+
+nba_total_stats <- nba_total_stats %>%
+  rename(fg_tot = "fg",
+         fga_tot = "fga",
+         x3p_tot = "x3p",
+         x3pa_tot = "x3pa",
+         x2p_tot = "x2p",
+         x2pa_tot = "x2pa",
+         ft_tot = "ft",
+         fta_tot = "fta",
+         orb_tot = "orb",
+         trb_tot = "trb",
+         ast_tot = "ast",
+         stl_tot = "stl",
+         blk_tot = "blk",
+         tov_tot = "tov",
+         pf_tot = "pf",
+         pts_tot = "pts"
+  )
+
 
 
 # Joining all the datasets together
@@ -19,9 +41,10 @@ nba_adv_stats <- read_csv("data/nba_adv_stats.csv")
 nba_all_stats <- nba_per100_stats %>%
   left_join(nba_adv_stats) %>%
   left_join(nba_shooting) %>%
-  left_join(nba_pbp) %>% 
-  arrange(-season) %>% 
-  select(1:4, 32, 5:30, 33:90, 31) %>%
+  left_join(nba_pbp) %>%
+  left_join(nba_total_stats) %>% 
+  arrange(-season) %>%  
+  select(1:4, 32, 5:30, 33:107, 31) %>%
   select(-rk)
 
 ### DOCUMENTATION
@@ -29,6 +52,7 @@ nba_all_stats <- nba_per100_stats %>%
 # Columns 32-51 are advanced stats - TS%, PER, WS, vorp, BPM, etc
 # Columns 52-72 are shootin tendencies - % of shots that are 2s, 3s, distance where they're taken, etc
 # Columns 73-88 are play-by-play data: on off, turnover type, % of minutes at each estimated position
+# Columns 89-105 are raw totals (2pm, 2pa, etc)
 
 # Write to csv
 # write_csv(nba_all_stats, "data/nba_all_stats.csv")
---
+++ andrew/Data Compilation/wnba-totals-scraping.R
@@ -0,0 +1,62 @@
+# PURPOSE: Create function to pull raw totals of players for given season from basketball-reference.com WNBA page
+# 
+# Cont: this will help cluster by player tendencies because we can use total attempts rather than percentages, etc
+
+
+# Load in data/libraries --------------------------------------------------
+
+library(rvest)
+library(tidyverse)
+
+season = 2022
+
+WNBATotalStatistics <- function(season = 2018) {
+  wnba_url <- paste("https://www.basketball-reference.com/wnba/years/",
+                    season,
+                    "_totals.html",
+                    sep = "")
+  pg <- rvest::read_html(wnba_url)
+  
+  wnba_stats <- rvest::html_table(pg, fill = T)[[1]]
+  
+  if (utils::packageVersion("janitor") > "0.3.1") {
+    wnba_stats <- wnba_stats %>%
+      janitor::clean_names(case = "old_janitor") %>%
+      dplyr::filter(.data$player != "Player")
+  } else {
+    wnba_stats <- wnba_stats %>%
+      janitor::clean_names() %>%
+      janitor::remove_empty_cols() %>%
+      dplyr::filter(.data$player != "Player")
+  }
+  
+  links <- pg %>%
+    rvest::html_nodes("tr.full_table") %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_attr("href")
+  
+  link_names <- pg %>%
+    rvest::html_nodes("tr.full_table") %>%
+    rvest::html_nodes("a") %>%
+    rvest::html_text()
+  
+  links_df <- dplyr::tibble(player = as.character(link_names),
+                            link   = as.character(links))
+  links_df[] <- lapply(links_df, as.character)
+  wnba_stats <- dplyr::left_join(wnba_stats, links_df, by = "player")
+  wnba_stats <- dplyr::mutate_at(wnba_stats,
+                                 dplyr::vars(-.data$player, -.data$pos, -.data$team, -.data$link),
+                                 list(as.numeric))
+  return(wnba_stats)
+}
+
+# Create totals dataset from 2018 to 2022
+wnba_total_stats <- tibble()
+for (i in 2018:2022) {
+  temp <- WNBATotalStatistics(i) %>%
+    mutate(season = i)
+  wnba_total_stats <- bind_rows(wnba_total_stats, temp)
+}
+
+# WRITE CSV
+write_csv(wnba_total_stats, "data/wnba_total_stats.csv")
