2c80452f2fb28d992455ca720b8a888177638fc2
Amor Ai <aa@macbook-pro-13.wifi.local.cmu.edu>
2022-07-18 00:27:20 -0400

    PCA on new dataset - need to deal with highly correlated variables

--- amor/update_pca.R
+++ amor/update_pca.R
@@ -35,18 +35,90 @@ wnba_2021_stats <- wnba_2021_stats %>%
 
 
 # PCA with total numeric variables (no %'s) ----------------------------------------
+library(broom)
+library(stringr)
+library(factoextra)
+
 
 wnba_numeric <- wnba_2021_stats %>% 
+  select_if(is.numeric) 
   
-  
-  
-  # select_if(is.numeric) %>% 
-  # mutate_all(~replace_na(.,0))
+#remove columns with percentages/containing na's
+model_x <- as.matrix(dplyr::select(wnba_numeric, 
+                                   -season, -g, -mp, -gs, -mpg,
+                                   -fgpercent, -x3ppercent, -x2ppercent, -ftpercent,
+                                   -fga_2p, -fga_0_3, -fga_3_10, -fga_10_16,
+                                   -fga_16_3p, -fga_3p, -fg_2p, -fg_0_3, -fg_3_10, 
+                                   -fg_10_16, -fg_16_3p, -fg_3p, -fg_2p_asst, -fg_2p_asst,
+                                   -corner_3s_attempt, -corner_3s_percent, -fg_3p_asst))
 
-#model_x <- as.matrix(dplyr::select(wnba_numeric, 
-                                   -season, -g, -mp, -gs, -mpg))
+pca_wnba <- prcomp(model_x, center = TRUE, scale = TRUE)
 
-#pca_wnba <- prcomp(model_x, center = TRUE, scale = TRUE)
+
+# PC's proportion of variance explained -----------------------------------
+
+get_eig(pca_wnba) #top 10 dimensions explain 90% of the variability
+
+pca_wnba %>%
+  tidy(matrix = "eigenvalues") %>%
+  ggplot(aes(x = PC, y = percent)) +
+  geom_line() + geom_point() +
+  geom_hline(yintercept = 1 / ncol(model_x),
+             color = "darkred", 
+             linetype = "dashed") +
+  theme_bw()
+
+fviz_eig(pca_wnba) #scree plot
+
+##Extra biplot
+# fviz_pca_biplot(pca_wnba, label = "var",  
+#                 alpha.ind = .5, col.var = "darkblue",
+#                 alpha.var = .75)
+
+
+#Table of variables and PC's
+
+pca_var <- pca_wnba %>%
+  # Extract variable coordinates
+  tidy(matrix = "rotation") %>%
+  # Format table form long to wide
+  pivot_wider(names_from = "PC", names_prefix = "PC", values_from = "value")%>%
+  # Rename column with variable names
+  rename(Variable=column) %>% 
+  # Take absolute value
+  mutate(across(PC1:PC61, ~ abs(.x))) 
+
+
+# Pick drivers in top 10 PC's (check for low correlations) --------------
+
+pc1 <- pca_var %>%  
+  select(Variable:PC1) %>% 
+  arrange(desc(PC1)) %>% 
+  slice(1:10)
+
+wnba_2021_stats %>% 
+  select(fg_tot, pts_tot, x2p_tot, x2pa_tot, shoot_fouls_drawn) %>% 
+  cor()
+
+# TOTALS STAT ARE ALL VERY CORRELATED WITH EACH OTHER!
+
+
+pc2 <- pca_var %>%  
+  select(c(Variable,PC2)) %>% 
+  arrange(desc(PC2)) %>% 
+  slice(1:5)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
